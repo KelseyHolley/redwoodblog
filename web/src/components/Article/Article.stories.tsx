2c90f94d80a2f282fec260b3878f963174559a92
Kelsey Holley <kdholley90@gmail.com>
2023-10-30 02:01:24 -0400

    Chapter 5

--- web/src/components/Article/Article.stories.tsx
+++ web/src/components/Article/Article.stories.tsx
@@ -10,7 +10,7 @@
 //
 // See https://storybook.js.org/docs/react/writing-stories/args.
 
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta } from '@storybook/react'
 
 import Article from './Article'
 
@@ -20,19 +20,20 @@ const meta: Meta<typeof Article> = {
 
 export default meta
 
-type Story = StoryObj<typeof Article>
+// type Story = StoryObj<typeof Article>
 
-export const Primary: Story = {}
+// export const Primary: Story = {}
 
-export const generated = () => {
-  return (
-    <Article
-      article={{
-        id: 1,
-        title: 'First Post',
-        body: 'Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Street art next level umami squid. Hammock hexagon glossier 8-bit banjo. Neutra la croix mixtape echo park four loko semiotics kitsch forage chambray. Semiotics salvia selfies jianbing hella shaman. Letterpress helvetica vaporware cronut, shaman butcher YOLO poke fixie hoodie gentrify woke heirloom.',
-        createdAt: '2020-01-01T12:34:56Z',
-      }}
-    />
-  )
+const ARTICLE = {
+  id: 1,
+  title: 'First Post',
+  body: 'Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Street art next level umami squid. Hammock hexagon glossier 8-bit banjo. Neutra la croix mixtape echo park four loko semiotics kitsch forage chambray. Semiotics salvia selfies jianbing hella shaman. Letterpress helvetica vaporware cronut, shaman butcher YOLO poke fixie hoodie gentrify woke heirloom.'
+}
+
+export const full = () => {
+  return <Article article={ARTICLE} />
+}
+
+export const summary = () => {
+  return <Article article={ARTICLE} summary={true} />
 }
--- web/src/components/Article/Article.test.tsx
+++ web/src/components/Article/Article.test.tsx
@@ -5,17 +5,29 @@ import Article from './Article'
 //   Improve this test with help from the Redwood Testing Doc:
 //    https://redwoodjs.com/docs/testing#testing-components
 
+const article = {
+  id: 1,
+  title: 'First post',
+  body: `Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Street art next level umami squid. Hammock hexagon glossier 8-bit banjo. Neutra la croix mixtape echo park four loko semiotics kitsch forage chambray. Semiotics salvia selfies jianbing hella shaman. Letterpress helvetica vaporware cronut, shaman butcher YOLO poke fixie hoodie gentrify woke heirloom.`,
+  createdAt: new Date().toISOString(),
+}
+
 describe('Article', () => {
   it('renders a blog post', () => {
-    const article = {
-      id: 1,
-      title: 'First post',
-      body: `Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Street art next level umami squid. Hammock hexagon glossier 8-bit banjo. Neutra la croix mixtape echo park four loko semiotics kitsch forage chambray. Semiotics salvia selfies jianbing hella shaman. Letterpress helvetica vaporware cronut, shaman butcher YOLO poke fixie hoodie gentrify woke heirloom.`,
-      createdAt: new Date().toISOString(),
-    }
     render(<Article article={article} />)
 
     expect(screen.getByText(article.title)).toBeInTheDocument()
     expect(screen.getByText(article.body)).toBeInTheDocument()
+  })
+
+  it('renders a summary of a blog post', () => {
+    render(<Article article={article} summary={true} />)
+
+    expect(screen.getByText(article.title)).toBeInTheDocument()
+    expect(
+      screen.getByText(
+        'Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Str...'
+      )
+    ).toBeInTheDocument()
   })
 })
